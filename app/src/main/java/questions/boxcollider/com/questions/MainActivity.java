ce51f3d5c68bae11feecf33bf11006ff103299c8
Aleksander <boxcollider@gmail.com>
2015-03-10 16:01:39 +0200

    edit math fragment layout

--- app/src/main/java/questions/boxcollider/com/questions/MainActivity.java
+++ app/src/main/java/questions/boxcollider/com/questions/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //ViewGroup parent = (ViewGroup) findViewById(R.id.parent);
+
+        getFragmentManager().beginTransaction().add(R.id.parent,new MathFragment(),"MATH").commit();
         //
     }
 
